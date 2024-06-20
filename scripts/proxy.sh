05e12b8352935937c4e05af44bcd98ade785ee94
Daniel Demmel <daniel.demmel@twist.eco>
2024-06-20 15:43:15 +0100

    Make it work with GOOGLE_APPLICATION_CREDENTIALS

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -4,11 +4,24 @@ set -euo pipefail
 PORT="${2}"
 INSTANCE_CONNECTION_NAME="${3}"
 TOKEN="${4:-null}"
+IMPLICIT_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS:-null}"
 DIR="/tmp/action-google-cloud-sql-proxy"
 IMAGE="gcr.io/cloud-sql-connectors/cloud-sql-proxy:${1}"
 
-if [ $TOKEN = "null" ]; then
-  # use credential file
+if [ $IMPLICIT_CREDENTIALS != "null" ]; then
+  echo "Starting proxy using GOOGLE_APPLICATION_CREDENTIALS"
+  docker run \
+    --detach \
+    --restart on-failure \
+    --name cloud-sql-proxy \
+    --publish "127.0.0.1:${PORT}:${PORT}" \
+    --volume "${IMPLICIT_CREDENTIALS}:${IMPLICIT_CREDENTIALS}" \
+    "${IMAGE}" \
+    ${INSTANCE_CONNECTION_NAME} \
+    --address 0.0.0.0 \
+    --credentials-file "${IMPLICIT_CREDENTIALS}"
+elif [ $TOKEN = "null" ]; then
+  echo "Starting proxy using credentials file"
   docker run \
     --detach \
     --restart on-failure \
@@ -20,7 +33,7 @@ if [ $TOKEN = "null" ]; then
     --address 0.0.0.0 \
     --credentials-file "${DIR}/key.json"
 else
-  # use token
+  echo "Starting proxy using token"
   docker run \
     --detach \
     --restart on-failure \
