d0b13e6eae554ee3652555dca7df50328df06594
Daniel Demmel <daniel.demmel@twist.eco>
2024-06-20 18:36:46 +0100

    Improve auth options conditions

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -8,7 +8,30 @@ IMPLICIT_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS:-null}"
 DIR="/tmp/action-google-cloud-sql-proxy"
 IMAGE="gcr.io/cloud-sql-connectors/cloud-sql-proxy:${1}"
 
-if [ $IMPLICIT_CREDENTIALS != "null" ]; then
+if [ $TOKEN != "null" ]; then
+  echo "Starting proxy using token"
+  docker run \
+    --detach \
+    --restart on-failure \
+    --name cloud-sql-proxy \
+    --publish "127.0.0.1:${PORT}:${PORT}" \
+    "${IMAGE}" \
+    ${INSTANCE_CONNECTION_NAME} \
+    --address 0.0.0.0 \
+    --token "${TOKEN}"
+elif [ -e ${DIR}/key.json ]; then
+  echo "Starting proxy using credentials file"
+  docker run \
+    --detach \
+    --restart on-failure \
+    --name cloud-sql-proxy \
+    --publish "127.0.0.1:${PORT}:${PORT}" \
+    --volume "${DIR}:${DIR}" \
+    "${IMAGE}" \
+    ${INSTANCE_CONNECTION_NAME} \
+    --address 0.0.0.0 \
+    --credentials-file "${DIR}/key.json"
+elif [ $IMPLICIT_CREDENTIALS != "null" ]; then
   echo "Starting proxy using GOOGLE_APPLICATION_CREDENTIALS"
   # make sure the container user will be permitted to use the JSON key file
   chmod a+r $GOOGLE_APPLICATION_CREDENTIALS
@@ -22,27 +45,7 @@ if [ $IMPLICIT_CREDENTIALS != "null" ]; then
     ${INSTANCE_CONNECTION_NAME} \
     --address 0.0.0.0 \
     --credentials-file "${IMPLICIT_CREDENTIALS}"
-elif [ $TOKEN = "null" ]; then
-  echo "Starting proxy using credentials file"
-  docker run \
-    --detach \
-    --restart on-failure \
-    --name cloud-sql-proxy \
-    --publish "127.0.0.1:${PORT}:${PORT}" \
-    --volume "${DIR}:${DIR}" \
-    "${IMAGE}" \
-    ${INSTANCE_CONNECTION_NAME} \
-    --address 0.0.0.0 \
-    --credentials-file "${DIR}/key.json"
 else
-  echo "Starting proxy using token"
-  docker run \
-    --detach \
-    --restart on-failure \
-    --name cloud-sql-proxy \
-    --publish "127.0.0.1:${PORT}:${PORT}" \
-    "${IMAGE}" \
-    ${INSTANCE_CONNECTION_NAME} \
-    --address 0.0.0.0 \
-    --token "${TOKEN}"
+  echo "Couldn't find valid credentials!"
+  exit 1
 fi
